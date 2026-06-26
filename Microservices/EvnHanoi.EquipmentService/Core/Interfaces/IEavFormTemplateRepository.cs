915e70dbd4c88b18b6adadf78cacbf07d923ee6f
hungpv <hungpv@ecoit.com.vn>
2026-06-26 16:32:08 +0700

    fix bug cau hinh form

--- Microservices/EvnHanoi.EquipmentService/Core/Interfaces/IEavFormTemplateRepository.cs
+++ Microservices/EvnHanoi.EquipmentService/Core/Interfaces/IEavFormTemplateRepository.cs
@@ -5,7 +5,8 @@ namespace EvnHanoi.EquipmentService.Core.Interfaces;
 public interface IEavFormTemplateRepository
 {
     Task<EavFormTemplate?> GetByIdAsync(Guid id);
-    Task<IEnumerable<EavFormTemplate>> GetAllActiveAsync(string? formType = null);
+    Task<IEnumerable<EavFormTemplate>> GetAllActiveAsync(string? formType = null, bool? isActive = true);
     Task AddAsync(EavFormTemplate template);
     Task UpdateAsync(EavFormTemplate template);
+    Task<IEnumerable<EavFormTemplate>> GetVersionsByCodeAsync(string code);
 }
