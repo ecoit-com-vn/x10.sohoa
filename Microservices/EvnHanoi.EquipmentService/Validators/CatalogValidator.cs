ebae41f8247e47cfc58e351908677efea50510c5
tanvantran <tanvantrankmahv@gmail.com>
2026-07-24 16:02:25 +0700

    feat: Thêm menu chức năng Quản lý danh mục > Danh mục mục lục hồ sơ. Danh mục có phân theo đơn vị, có thể tạo dạng cha con

--- Microservices/EvnHanoi.EquipmentService/Validators/CatalogValidator.cs
+++ Microservices/EvnHanoi.EquipmentService/Validators/CatalogValidator.cs
@@ -1,5 +1,5 @@
-using FluentValidation;
 using EvnHanoi.EquipmentService.Core.Entities;
+using FluentValidation;
 
 namespace EvnHanoi.EquipmentService.Validators;
 
@@ -7,9 +7,19 @@ public class CatalogValidator : AbstractValidator<Catalog>
 {
     public CatalogValidator()
     {
-        RuleFor(c => c.Code)
-            .NotEmpty().WithMessage("Mã danh mục không được để trống.");
-        RuleFor(c => c.Name)
-            .NotEmpty().WithMessage("Tên danh mục không được để trống.");
+        RuleFor(x => x.Code)
+            .NotEmpty().WithMessage("Mã danh mục là bắt buộc")
+            .MaximumLength(50).WithMessage("Mã danh mục không được vượt quá 50 ký tự");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Tên danh mục là bắt buộc")
+            .MaximumLength(255).WithMessage("Tên danh mục không được vượt quá 255 ký tự");
+
+        RuleFor(x => x.CatalogTypeId)
+            .GreaterThan(0).WithMessage("Loại danh mục không hợp lệ");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000).WithMessage("Mô tả không được vượt quá 1000 ký tự")
+            .When(x => !string.IsNullOrEmpty(x.Description));
     }
 }
