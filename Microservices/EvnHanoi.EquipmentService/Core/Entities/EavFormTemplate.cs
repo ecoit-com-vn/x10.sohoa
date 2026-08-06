cc789d616f9d31e35ee03f91ae460baabfcbf6c9
phuruytho28 <cufau13@gmail.com>
2026-08-06 16:33:39 +0700

    fix pagniation

--- Microservices/EvnHanoi.EquipmentService/Core/Entities/EavFormTemplate.cs
+++ Microservices/EvnHanoi.EquipmentService/Core/Entities/EavFormTemplate.cs
@@ -41,7 +41,5 @@ public class EavFormTemplateFilterDto
     public string? Keyword { get; set; } 
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; } 
-    public string? Status { get; set; }
-    public int Page { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
+    public string? Status { get; set; } 
 }
