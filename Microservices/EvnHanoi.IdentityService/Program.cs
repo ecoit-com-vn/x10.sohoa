c9aa2a9f21eed5ce88752c628f03c6c128a6ff09
hungpv <hungpv@ecoit.com.vn>
2026-09-18 14:26:17 +0700

    config api

--- Microservices/EvnHanoi.IdentityService/Program.cs
+++ Microservices/EvnHanoi.IdentityService/Program.cs
@@ -51,6 +51,7 @@ builder.Services.AddScoped<IUserUnitRoleRepository, UserUnitRoleRepository>();
 builder.Services.AddScoped<IUploadConfigRepository, UploadConfigRepository>();
 builder.Services.AddScoped<IExternalApiKeyRepository, ExternalApiKeyRepository>();
 builder.Services.AddScoped<IExternalApiCallLogRepository, ExternalApiCallLogRepository>();
+builder.Services.AddScoped<IExternalApiKeyValidator, ExternalApiKeyValidator>();
 builder.Services.AddSingleton<IExternalApiKeyProtector, ExternalApiKeyProtector>();
 builder.Services.AddScoped<IPermissionRepository, PermissionRepository>();
 builder.Services.AddScoped<IAvatarStorageService, AvatarStorageService>();
